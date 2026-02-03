3095815821eb50f46d997f4d7be4ef1d0a5acf99
Andrei Railean <andrei@siter.com.au>
2026-02-03 16:38:55 +1100

    move everything into one cli file. rename image

--- configs/init.lua
+++ configs/init.lua
@@ -1,1 +1,11 @@
+local project_state = "/app/.dev-env-container-state"
+
+if vim.fn.isdirectory(project_state) == 0 then
+	vim.fn.mkdir(project_state, "p")
+end
+
+vim.opt.undodir = project_state .. "/undo//"
+vim.opt.swapfile = true
+vim.opt.directory = project_state .. "/swap//"
+
 vim.opt.number = true
